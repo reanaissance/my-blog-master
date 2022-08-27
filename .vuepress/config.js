c7b5dc0d4fd0061e84666cae0d4db68dcae56a5a
wangxuan <15135921427@163.com>
2022-08-28 01:50:52 +0800

    first commit

---
+++ .vuepress/config.js
@@ -0,0 +1,99 @@
+module.exports = {
+  "title": "山远风起",
+  "description": "blog start",
+  "dest": "public",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "Home",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "TimeLine",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "环境搭建",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "build",
+            "link": "/docs/build/"
+          }
+        ]
+      },
+      {
+        "text": "Contact",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "GitHub",
+            "link": "https://github.com/recoluan",
+            "icon": "reco-github"
+          }
+        ]
+      }
+    ],
+    "sidebar": {
+      "/docs/build/": [
+        "",
+        "vuePress"
+      ]
+    },
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "Category"
+      },
+      "tag": {
+        "location": 3,
+        "text": "Tag"
+      }
+    },
+    "friendLink": [
+      {
+        "title": "午后南杂",
+        "desc": "Enjoy when you can, and endure when you must.",
+        "email": "1156743527@qq.com",
+        "link": "https://www.recoluan.com"
+      },
+      {
+        "title": "vuepress-theme-reco",
+        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
+        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+        "link": "https://vuepress-theme-reco.recoluan.com"
+      }
+    ],
+    "logo": "/logo.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "lastUpdated": "Last Updated",
+    "author": "山远风起",
+    "authorAvatar": "/avatar.png",
+    "record": "xxxx",
+    "startYear": "2017"
+  },
+  "markdown": {
+    "lineNumbers": true
+  }
+}
---
+++ docs/build/vuePress.md
@@ -0,0 +1,34 @@
+# vuePress搭建部署
+
+#### 安装theme-cli命令
+
+```shell
+npm install @vuepress-reco/theme-cli -g 
+#-g 代表全局安装
+```
+
+#### 初始化本地项目
+
+```shell
+theme-cli init '项目名称'
+```
+
+#### 安装依赖
+
+```shell
+cd '项目路径'
+npm install
+```
+
+#### 启动项目
+
+```shell
+npm run dev
+```
+
+#### 打包命令
+
+```shell
+npm run build
+```
+
